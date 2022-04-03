4800f3e819557292706cacbc86031bb33cd2e3b5
ge34bec <an.nguyen@tum.de>
2022-04-04 00:02:26 +0200

    sa

--- pages/company/contact.js
+++ pages/company/contact.js
@@ -1,76 +1,76 @@
-import React, { useState, useMemo } from 'react'
+// import React, { useState, useMemo } from 'react'
 
 
-import {db} from '../../config/firebase'
-import {collection, addDoc} from "firebase/firestore"
+// import {db} from '../../config/firebase'
+// import {collection, addDoc} from "firebase/firestore"
 
-// import style from '../../styles/Contact.module.css'
+// // import style from '../../styles/Contact.module.css'
 
-const contact = () => {
-  const [firstName,setFirstName] = useState('')
-  const [lastName,setLastName] = useState('')
-  const [email, setEmail]= useState('')
+// const contact = () => {
+//   const [firstName,setFirstName] = useState('')
+//   const [lastName,setLastName] = useState('')
+//   const [email, setEmail]= useState('')
 
-  const [company, setCompany] = useState('')
-  const [subject,setSubject] = useState('')
-  const [message,setMessage] = useState('')
+//   const [company, setCompany] = useState('')
+//   const [subject,setSubject] = useState('')
+//   const [message,setMessage] = useState('')
 
   
-  const formCollectionRef = collection(db,"ContactForm")
+//   const formCollectionRef = collection(db,"ContactForm")
 
 
 
 
-const firstNameHandler = value => {
-  setFirstName(value)
-}
-const lastNameHandler = value => {
-  setLastName(value)
-}
-const emailHandler = value => {
-  setEmail(value)
-}
-const companyHandler = value => {
-  setCompany(value)
+// const firstNameHandler = value => {
+//   setFirstName(value)
+// }
+// const lastNameHandler = value => {
+//   setLastName(value)
+// }
+// const emailHandler = value => {
+//   setEmail(value)
+// }
+// const companyHandler = value => {
+//   setCompany(value)
   
-}
+// }
 
-const subjectHandler = value => {
-  setSubject(value)
-}
+// const subjectHandler = value => {
+//   setSubject(value)
+// }
 
-const messageHandler = value => {
-  setMessage(value)
-}
-const createMessage = async(firstName,lastName,email,country,subject, message)=>{
-  await addDoc(formCollectionRef,{
-    firstName:firstName,
-    lastName:lastName,
-    email:email,
-    country:country,
-    subject:subject,
-    message:message})
-}
+// const messageHandler = value => {
+//   setMessage(value)
+// }
+// const createMessage = async(firstName,lastName,email,country,subject, message)=>{
+//   await addDoc(formCollectionRef,{
+//     firstName:firstName,
+//     lastName:lastName,
+//     email:email,
+//     country:country,
+//     subject:subject,
+//     message:message})
+// }
 
-const submitMessage =()=>{
-    if(firstName===''||lastName===''||email===''|| subject===''|| message===''){
-      alert('Invalid form')
-    } else {
-      createMessage(firstName,lastName,email,country.label,subject, message)
-      // alert('sent successful')
-    }
+// const submitMessage =()=>{
+//     if(firstName===''||lastName===''||email===''|| subject===''|| message===''){
+//       alert('Invalid form')
+//     } else {
+//       createMessage(firstName,lastName,email,country.label,subject, message)
+//       // alert('sent successful')
+//     }
   
  
 
-} 
+// } 
 
 
-  return (
-    <div className='min-h-screen w-full'>
+//   return (
+//     <div className='min-h-screen w-full'>
 
-    </div>
-  )
-}
+//     </div>
+//   )
+// }
 
-export default contact
+// export default contact
 
